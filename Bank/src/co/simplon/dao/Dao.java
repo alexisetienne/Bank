35fdb6590b2a72901d116eda9aa70244a31d1365
mohamed el babili <elbabilimohamed@gmail.com>
2019-03-14 06:58:31 +0100

    exemple javadoc

--- Bank/src/co/simplon/dao/Dao.java
+++ Bank/src/co/simplon/dao/Dao.java
@@ -1,6 +1,11 @@
 package co.simplon.dao;
-
 import java.sql.Connection;
+
+/** 
+ * Application de gestion bancaire, partie DAO
+ * @version 1.0
+ * @author El babili
+ */
 
 public abstract class Dao<T> {
 
@@ -9,7 +14,7 @@ public abstract class Dao<T> {
 	/**
 	 * Permet de récupérer un objet via son ID
 	 * @param id
-	 * @return
+	 * @return T
 	 */
 	public abstract T find(int id);
 	
@@ -17,18 +22,21 @@ public abstract class Dao<T> {
 	 * Permet de créer une entrée dans la base de données
 	 * par rapport à un objet
 	 * @param obj
+	 * @return boolean
 	 */
 	public abstract boolean create(T obj);
 	
 	/**
 	 * Permet de mettre à jour les données d'une entrée dans la base 
 	 * @param obj
+	 * @return boolean
 	 */
 	public abstract boolean update(T obj);
 	
 	/**
 	 * Permet la suppression d'une entrée de la base
 	 * @param obj
+	 * @return boolean
 	 */
 	public abstract boolean delete(T obj);
 }
